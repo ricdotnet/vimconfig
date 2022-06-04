e8f5d2fa3b4f9f4d3e9df7d7517b3f4d4cc70edb
Ricardo Rocha <me@rrocha.uk>
2022-06-04 15:29:40 +0100

    * some more configs;

--- install.sh
+++ install.sh
@@ -1,21 +1,7 @@
 mkdir ~/bin # directory to help on installing language servers
-
-# tailwind intellisense ... check for latest version
-curl -L https://github.com/tailwindlabs/tailwindcss-intellisense/releases/download/v0.8.6/vscode-tailwindcss-0.8.6.vsix -o tailwindcss-intellisense.vsix
-
-unzip -d ~/bin/ tailwindcss-intellisense.vsix
-cd ~/bin
-rm \[Content_Types\].xml extension.vsixmanifest
-chmod +x extension/dist/server/index.js
-
-cat <<EOF > tailwindcss-language-server
-#!/usr/bin/env bash
- 
-DIR=\$(cd \$(dirname \$0); pwd)
-node \$DIR/extension/dist/server/index.js \$*
-EOF
- 
-chmod +x tailwindcss-language-server
+export PATH=$PATH:~/bin
 
 # language servers
 npm install -g typescript typescript-language-server
+npm install -g @tailwindcss/language-server
+npm install -g vscode-langservers-extracted
