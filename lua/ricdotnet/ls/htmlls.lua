66eec448c69534ef64f16f63698dba3aef6d1077
Ricardo Rocha <me@rrocha.uk>
2023-08-13 19:53:45 +0100

    added snippets and jinja ls to emmet

--- lua/ricdotnet/ls/htmlls.lua
+++ lua/ricdotnet/ls/htmlls.lua
@@ -2,6 +2,6 @@ return function(capabilities, on_attach)
   require("lspconfig")["emmet_language_server"].setup {
     on_attach = on_attach,
     capabilities = capabilities,
-    filetypes = { "html", "njk" },
+    filetypes = { "html", "njk", "jinja" },
   }
 end
--- lua/ricdotnet/lsp.lua
+++ lua/ricdotnet/lsp.lua
@@ -1,19 +1,17 @@
+require("luasnip.loaders.from_snipmate").lazy_load({ paths = { "./snippets" } })
+
 local function on_attach()
   local cmp = require("cmp")
 
   cmp.setup({
     snippet = {
-      -- REQUIRED - you must specify a snippet engine
       expand = function(args)
-        --vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
         require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-        -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-        -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
       end,
     },
     window = {
-      -- completion = cmp.config.window.bordered(),
-      -- documentation = cmp.config.window.bordered(),
+      completion = cmp.config.window.bordered(),
+      documentation = cmp.config.window.bordered(),
     },
     mapping = cmp.mapping.preset.insert({
       ['<C-b>'] = cmp.mapping.scroll_docs(-4),
@@ -24,8 +22,7 @@ local function on_attach()
     }),
     sources = cmp.config.sources({
       { name = 'nvim_lsp' },
-      -- { name = 'vsnip' }, -- For vsnip users.
-      { name = 'luasnip' }, -- For luasnip users.
+      { name = 'luasnip' },
       { name = 'path' },
     }, {
       { name = 'buffer' },
