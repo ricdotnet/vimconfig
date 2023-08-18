cd384e6f582d3de93cda65d50bdb3d7657b3fe16
Ricardo Rocha <me@rrocha.uk>
2023-08-18 17:20:40 +0100

    contrast

--- lua/ricdotnet/themes/material.lua
+++ lua/ricdotnet/themes/material.lua
@@ -1,5 +1,12 @@
 require("material").setup({
-
+  contrast = {
+    terminal = true,                -- Enable contrast for the built-in terminal
+    sidebars = true,                -- Enable contrast for sidebar-like windows ( for example Nvim-Tree )
+    floating_windows = true,        -- Enable contrast for floating windows
+    cursor_line = true,             -- Enable darker background for the cursor line
+    non_current_windows = true,     -- Enable contrasted background for non-current windows
+    filetypes = {},                  -- Specify which filetypes get the contrasted (darker) background
+  },
 })
 
 vim.g.material_style = "deep ocean"
